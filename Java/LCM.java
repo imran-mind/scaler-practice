327a9b097027c3a973debccf1d8498c75df0d99d
Shaikh Imran <shaikhimran115@gmail.com>
2022-01-24 20:23:59 +0530

    lcm added

---
+++ Java/LCM.java
@@ -0,0 +1,19 @@
+public class LCM {
+
+    public  static int gcdImproved(int a,int b){
+        return b == 0 ?  a : gcdImproved(b, a%b);
+    }
+
+    public static int lcm(int a, int b){
+        int l = a*b/gcdImproved(a,b);
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 9;
+//        int res = gcd(a,b);
+        int res = lcm(a,b);
+        System.out.println(res);
+    }
+}
