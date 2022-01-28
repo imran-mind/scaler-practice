6f7a8e552d51eb7a2f92fa0fc57a7746ee9fca33
Shaikh Imran <shaikhimran115@gmail.com>
2022-01-28 15:12:06 +0530

    remove dup and give count of unique

---
+++ ScalerTest/array/RemoveDupInPlace.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+public class RemoveDupInPlace {
+
+    public static int removeDuplicates(int[] nums){
+        if(nums.length == 0)
+            return 0;
+        int i = 0;
+        for(int j=1; j<nums.length; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args) {
+        int[] a = {1,1,2};
+        System.out.println(removeDuplicates(a));
+        System.out.println(Arrays.toString(a));
+    }
+}
