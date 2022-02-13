06c9fc0bef2a25bf0029c2b84b0c9ee235536358
Shaikh Imran <shaikhimran115@gmail.com>
2022-02-13 12:31:46 +0530

    sum of digits

---
+++ ScalerTest/AddDigits.java
@@ -0,0 +1,16 @@
+public class AddDigits {
+
+    public static int addDigits(int num) {
+        if(num <= 9 ){
+            return num;
+        }
+        int a = num/10;
+        int b = num%10;
+        int sum = a+b;
+        return addDigits(sum);
+    }
+    public static void main(String[] args) {
+        int a = 100;// 38
+        System.out.println(addDigits(a));
+    }
+}
