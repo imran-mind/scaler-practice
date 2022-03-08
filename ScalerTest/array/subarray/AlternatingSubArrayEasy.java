478bc453485a02567afce1f3ef46bcc4c791d24e
Shaikh Imran <shaikhimran115@gmail.com>
2022-03-08 12:19:37 +0530

    Update AlternatingSubArrayEasy.java

--- ScalerTest/array/subarray/AlternatingSubArrayEasy.java
+++ ScalerTest/array/subarray/AlternatingSubArrayEasy.java
@@ -1,6 +1,7 @@
 package array.subarray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AlternatingSubArrayEasy {
 
@@ -34,9 +35,56 @@ public class AlternatingSubArrayEasy {
         return list;
     }
 
+    public static int[] solve1(int[] arr, int b){
+        int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=b; i<n; i++){
+            if(b>0){
+                int k = b;
+                boolean isValidLeft = false;
+                boolean isValidRight = false;
+                for(int j=i; j>0; j--){
+                    if(k>0){
+                        if(arr[j] != arr[j-1]){
+                            isValidLeft = true;
+                        }else {
+                            isValidLeft = false;
+                            break;
+                        }
+                    }
+                    k--;
+                }
+                int p = b;
+                for(int j=i; j<n-b; j++){
+                    if(p>0){
+                        if(arr[j] != arr[j+1]){
+                            isValidRight = true;
+                        }else {
+                            isValidRight = false;
+                            break;
+                        }
+                    }
+                    p--;
+                }
+//                System.out.println("i => "+i +" isValidLeft "+isValidLeft);
+                if(isValidLeft && isValidRight){
+                    list.add(i);
+                }
+            }else{
+                list.add(i);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1,0,1,0,1};
+        int[] arr =  {1,0,1,0,1}; //{0, 0, 0, 1, 1, 0, 1};//{1, 1, 0, 1, 1, 0, 1};//{1,0,1,0,1}; //
+//        int[] arr = {1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1 };
         int B = 1;
-        System.out.println(solve(arr,B));
+        System.out.println(Arrays.toString(solve1(arr,B)));
     }
 }
