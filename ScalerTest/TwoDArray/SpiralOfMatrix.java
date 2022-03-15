c7cc0d6199c46deca8ce0db56457c21d2667ebcb
Shaikh Imran <shaikhimran115@gmail.com>
2022-03-15 21:56:48 +0530

    Update SpiralOfMatrix.java

--- ScalerTest/TwoDArray/SpiralOfMatrix.java
+++ ScalerTest/TwoDArray/SpiralOfMatrix.java
@@ -46,11 +46,57 @@ public class SpiralOfMatrix {
 
         return ans;
     }
+
+
+    // Given an integer A, generate a square matrix filled with elements from 1 to A^2 in spiral order
+    public static int[][] matrixInSpiralOrderOptimizedApproach(int A){
+        int counter = 1;
+        int[][] mat = new int[A][A];
+        int i=0,j=0;
+
+        while (A>1){
+            //Left ro Right , row is constant
+            for(int k=1; k<A; k++){
+                mat[i][j] = counter;
+                counter++;
+                j++;
+            }
+            //Top to Bottom , col is constant
+            for(int k=1; k<A; k++){
+                mat[i][j] = counter;
+                counter++;
+                i++;
+            }
+            //Left to Right, row is constant
+            for(int k=1; k<A; k++){
+                mat[i][j] = counter;
+                counter++;
+                j--;
+            }
+            for(int k=1; k<A; k++){
+                mat[i][j] = counter;
+                counter++;
+                i--;
+            }
+            A = A - 2;
+            i++;
+            j++;
+        }
+        if(A == 1){
+            mat[i][j] = counter;
+        }
+        return mat;
+    }
     public static void main(String[] args) {
         int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         int r = 4, c= 4;
-        ArrayList<Integer> ans = spiralOfMat(arr,r,c);
+//        ArrayList<Integer> ans = spiralOfMat(arr,r,c);
 
-        System.out.println(ans);
+//        System.out.println(ans);
+        int[][] mat = matrixInSpiralOrderOptimizedApproach(4);
+
+        for(int i=0; i<mat.length; i++){
+            System.out.println(Arrays.toString(mat[i]));
+        }
     }
 }
