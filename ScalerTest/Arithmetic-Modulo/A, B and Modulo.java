516efca326c7eb5d7c7d15fd86e75e5c7f793923
Shaikh Imran <shaikhimran115@gmail.com>
2022-03-19 10:15:58 +0530

    Create A, B and Modulo.java

---
+++ ScalerTest/Arithmetic-Modulo/A, B and Modulo.java
@@ -0,0 +1,9 @@
+/*https://www.scaler.com/academy/mentee-dashboard/class/19185/assignment/problems/5840?navref=cl_tt_nv*/
+public class Solution {
+    public int solve(int A, int B) {
+        if(A>B)
+            return A-B;
+        else
+            return B-A;
+    }
+}
