7a8247b1a3bba1da227fa4daa7f56be1c29eb551
Shaikh Imran <shaikhimran115@gmail.com>
2022-04-06 12:05:23 +0530

    Update SumTheDigits.java

--- ScalerTest/recursion/SumTheDigits.java
+++ ScalerTest/recursion/SumTheDigits.java
@@ -10,6 +10,12 @@ public class SumTheDigits {
         n = n/10;
         return sumOfDig(n);
     }
+    // good approach
+    public int solve(int A) {
+        if(A<10)
+            return A;
+        return (A%10) + solve(A/10);
+    }
     public static void main(String[] args) {
         int n = 1589;//123;
         int result = sumOfDig(n);
